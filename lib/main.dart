dcec61217a8dc5b1daa55f664b2abeb7bf8580df
Ali Jawad <jawadsubhan7@gmail.com>
2023-04-22 09:53:47 +0500

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class EidMubarak extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: EidMubarakUI(),
     );
   }
